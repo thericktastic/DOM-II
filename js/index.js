48907374c70ec740c5e721d4c7218d92fe4e08d2
Rick Tastic <love@The-RT-MBP.attlocal.net>
2019-12-03 20:50:11 -0500

    Finished events and stop propagation

--- js/index.js
+++ js/index.js
@@ -22,7 +22,7 @@ funBusLogo.addEventListener("mouseout", () => {
 document.querySelectorAll(".nav-link").forEach(function(element) {
   element.addEventListener("click", event => {
     event.preventDefault();
-    console.log("halted the enemy's movement")
+    console.log("halted the enemy's movement");
   });
 });
 
@@ -36,41 +36,64 @@ document.querySelectorAll(".nav-link").forEach(function(element) {
 
 // Event 4 - mouseenter
 funBusHdrImg.addEventListener("mouseenter", () => {
-    funBusHdrImg.style.filter = "grayscale(100%)";
-    funBusHdrImg.style.transition = "filter 2s";
-})
+  funBusHdrImg.style.filter = "grayscale(100%)";
+  funBusHdrImg.style.transition = "filter 2s";
+});
 
 // Event 5 - mouseleave
 funBusHdrImg.addEventListener("mouseleave", () => {
-    funBusHdrImg.style.filter = "none";
-})
+  funBusHdrImg.style.filter = "none";
+});
 
 // Event 6 - contextmenu
 funBusHdrImg.addEventListener("contextmenu", () => {
-    funBusHdrImg.style.transform = "rotate(360deg)";
-    funBusHdrImg.style.transition = "transform 3s";
-})
+  funBusHdrImg.style.transform = "rotate(360deg)";
+  funBusHdrImg.style.transition = "transform 3s";
+});
 
 // Event 7 - dblclick
 mapImg.addEventListener("dblclick", () => {
-    mapImg.style.transform = "scale(1.5)";
-    mapImg.style.transition = "transform 2s";
-})
+  mapImg.style.transform = "scale(1.5)";
+  mapImg.style.transition = "transform 2s";
+});
 
 // Event 8 - keydown
 document.addEventListener("keydown", () => {
-    selectPick.style.color = "red";
-})
+  selectPick.style.color = "red";
+});
 
 // Event 9 - scroll
-document.querySelectorAll("img").forEach((element) => {
-    document.addEventListener("scroll", () => {
-      element.style.transform = "scale(1.1)";
-      element.style.transition = "transform 3s";
-    });
+document.querySelectorAll("img").forEach(element => {
+  document.addEventListener("scroll", () => {
+    element.style.transform = "scale(1.1)";
+    element.style.transition = "transform 3s";
   });
+});
 
 // Event 10 - keyup
 document.addEventListener("keyup", () => {
-    footer.style.backgroundColor = "#FFD800";
-})
+  footer.style.backgroundColor = "#FFD800";
+});
+
+// Stop propagation
+const body = document.querySelector("body");
+body.addEventListener("click", () => {
+  body.style.backgroundColor = "#FFD800";
+  console.log("me last!");
+});
+
+document.querySelectorAll(".destination").forEach(function(element) {
+  element.addEventListener("click", event => {
+    element.style.backgroundColor = "red";
+    console.log("me second!");
+    event.stopPropagation();
+  });
+});
+
+document.querySelectorAll("h4").forEach(function(element) {
+  element.addEventListener("click", event => {
+    element.style.backgroundColor = "pink";
+    console.log("me first!");
+    event.stopPropagation();
+  });
+});
